f601dc6b7e557ec266ddd046a639dc25a5b53255
Felix Breuer <felix@fbreuer.de>
2011-07-10 22:58:59 +0200

    build script tweak

--- compile.bat
+++ compile.bat
@@ -1,3 +1,8 @@
 set PATH=C:\Python27;%PATH%
+set OUTPUTDIR=chromeless\build\Qute\
 
 python chromeless\chromeless appify src
+
+copy LICENSE-AGPL %OUTPUTDIR%
+copy README.md %OUTPUTDIR%
+copy src\img\icon.ico %OUTPUTDIR%
