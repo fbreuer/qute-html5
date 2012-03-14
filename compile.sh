808898144391774372ada75b1398111e21d9f4c7
Felix Breuer <felix@fbreuer.de>
2012-03-14 13:08:37 -0700

    Qute 0.4.1 release

--- compile.sh
+++ compile.sh
@@ -7,3 +7,5 @@ python chromeless/chromeless appify src
 cp LICENSE-AGPL $OUTPUTDIR
 cp README.md $OUTPUTDIR
 cp src/img/icon.ico $OUTPUTDIR
+cp src/img/icon-512.png $OUTPUTDIR/qute.png
+cp Qute.desktop $OUTPUTDIR
--- src/default-txt.js
+++ src/default-txt.js
@@ -15,15 +15,20 @@ The **Q** in the top left corner opens the **menu**, which you can use to save a
 \n\
 ## News\n\
 \n\
-This release, Qute 0.4, is a small bugfix release. There are a few known issues, which are listed at http://github.com/fbreuer/qute-html5/issues.\n\
+This release, **Qute 0.4.1**, is the first in a series of **development releases**. The main new feature in this version is support for user-defined markup languages by way of OMeta. See below for more info.\n\
 \n\
-For the next release, Qute 0.5, a rewrite of large parts of Qute is\n\
-  planned, which means more features and a cleaner implementation at\n\
-  the expense of some convenience functions.\n\
+Qute is currently being extended to include a large number of new features, including\n\
 \n\
-## Custom Languages\n\
+* user-defined markup languages (see below),\n\
+* outlining,\n\
+* ink support, and\n\
+* Qute as a web-app.\n\
 \n\
-Qute allows you to define and transform your own custom markup languages. This feature is still very experimental, but here is a small demo. Consider the following calculator language called 'mylang', defined in OMeta syntax.\n\
+This process is going to take some time. It may involve both a rewrite of large parts of Qute and a change of the file format. The development releases represent a number of iterative steps toward that goal. Qute 0.4.1 is still backwards compatible with 0.4. The first release that breaks backwards compatibility will be 0.5.0.\n\
+\n\
+## User-defined Markup Languages\n\
+\n\
+Qute allows you to define and transform your own custom markup languages. This feature is still very experimental, but here is a small demo. Consider the following calculator language defined in OMeta syntax.\n\
 \n\
 @ometa:mylang\n\
 ometa Calc <: Parser {\n\
@@ -46,28 +51,32 @@ ometa Calc <: Parser {\n\
 Calc.initialize = function() {  this.vars = {}; }\n\
 function(source) {return Calc.matchAll(source,'doit',undefined,parseError)}\n\
 \n\
-Then, a paragraph containing an expression in this small caluclator language, such as for example\n\
+This calculator example is not very interesting to use with Qute. A more interesting example will be included in the next release of Qute. However, this example demonstrates nicely, that OMeta grammars can be used to define and use *custom markup languages* right inside your documents.\n\
+\n\
+If you click on the above paragraph of OMeta code, you see that, when in edit mode, you can let Qute know that this paragraph defines a grammar by placing the string `@ometa:<yourlang>` at the beginning of the paragraph. In this case, we chose to call the little calculator language 'mylang'. To use this custom grammar, we can use the prefix `@mylang`. For example\n\
  \n\
     @mylang\n\
     x=6\n\
     y=5+3\n\
     x*(y-1)\n\
  \n\
-will be automatically transformed into its result. The prefix @mylang tells Qute which language the paragraph is written in. Here is the result:\n\
+will be transformed using the above OMeta parser, which effectively returns the result of this little computation. Here is the result:\n\
 \n\
 @mylang\n\
 x=6\n\
 y=5+3\n\
 x*(y-1)\n\
 \n\
-You may note that the above language does not allow for unary negation. Entering a negative number will result in a parse error. Parse errors are reported like this:\n\
+If you click on this number, you can see the markup in our calculator language that is transformed into 42 using the above grammar. If you edit this paragraph and switch to display mode, you will see that the number is updated accordingly.\n\
+\n\
+Unless the markup you wrote does not parse. You may note, for example, that the above language does not allow for unary negation. Entering a negative number will result in a parse error. Parse errors are reported like this:\n\
 \n\
 @mylang\n\
 x=6\n\
 y=-5+3\n\
 x*(y-1)\n\
 \n\
-Check out the next development version of Qute for a more useful example of a custom language.\n\
+OMeta support is still very preliminary in Qute. Expect more to come in the next few iterations.\n\
 \n\
 ## Keyboard Shortcuts\n\
 \n\
